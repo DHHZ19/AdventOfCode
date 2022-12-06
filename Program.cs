eaf76a41c7cb3c9a1ce55943db144259408ea383
Diego Hernandez <diegofootball19@gmail.com>
2022-12-06 07:13:15 -0600

    why doesnt this work

--- Program.cs
+++ Program.cs
@@ -3,19 +3,6 @@
 public class Program
 {
     private static string txt = File.ReadAllText(@"./day02/input.txt");
-
-    // opponent:  A for Rock, B for Paper, and C for Scissors
-    static int A = 1;
-    static int B = 2;
-    static int C = 3;
-    //  You: X for Rock, Y for Paper, and Z for Scissors
-    static int X = 1;
-    static int Y = 2;
-    static int Z = 3;
-
-    static int Win = 6;
-    static int loss = 0;
-    static int draw = 3;
     static int totalScore = 0;
     static string[][] file = txt.Split("\n").Select(letter => letter.Split()).ToArray();
 
@@ -25,22 +12,20 @@ public class Program
         {
             string yourThrow = file[i][1];
             string thereThrow = file[i][0];
-            checkIfDraw(thereThrow, yourThrow);
             checkIfWin(thereThrow, yourThrow);
+            checkIfDraw(thereThrow, yourThrow);
 
             switch (yourThrow)
             {
                 case "X":
-                    totalScore = 1;
+                    totalScore += 1;
                     break;
                 case "Y":
-                    totalScore = 2;
+                    totalScore += 2;
                     break;
                 case "Z":
-                    totalScore = 3;
+                    totalScore += 3;
                     break;
-                default:
-                    return 0;
             }
         }
         return totalScore;
@@ -48,18 +33,31 @@ public class Program
 
     public static void checkIfWin(string thereThrow, string yourThrow)
     {
-        // win state
-        if (thereThrow == "A" && yourThrow == "X")
+        if (thereThrow == "C" && yourThrow == "X")
         {
             totalScore += 6;
 
+        }
+        else if (thereThrow == "A" && yourThrow == "Y")
+        {
+            totalScore += 6;
+
+        }
+        else if (thereThrow == "B" && yourThrow == "Z")
+        {
+            totalScore += 6;
+
+        }
+        else
+        {
+            totalScore += 0;
         }
     }
 
     public static void checkIfDraw(string thereThrow, string yourThrow)
     {
         // draw state
-        if (thereThrow == yourThrow)
+        if (yourThrow == thereThrow)
         {
             totalScore += 3;
         }
@@ -69,13 +67,6 @@ public class Program
 
     public static void Main(string[] args)
     {
-        calcScore(file);
-        // win and a loss state for each type
-
-        // shape you selected (1 for Rock, 2 for Paper, and 3 for Scissors)
-
-        // outcome of the round (0 if you lost, 3 if the round was a draw, and 6 if you won)
-
-        // creats a 2D array of the inputs 
+        Console.WriteLine(calcScore(file));
     }
 }
